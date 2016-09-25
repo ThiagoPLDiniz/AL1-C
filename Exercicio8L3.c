d910adea49fd7e77b7d8bbd8576d41cbab1e6987
Thiago Diniz <thiagopaesldiniz@gmail.com>
2016-09-25 17:15:32 -0300

    Add files via upload

---
+++ Exercicio8L3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main()
+{
+	float salario, salariom;
+	int idade, matricula, servico, fem=0, hom=0, i;
+	char sexo;
+	 
+	for(i=0; i<5; i++)
+	{
+		printf("Qual a sua idade?\n");
+		scanf("%d", &idade);
+		printf("Digite sua matricula:\n");
+		scanf("%d", &matricula);
+		printf("Qual o seu sexo?\n");
+		scanf("\n%c", &sexo);
+		printf("Qual o seu salário?\n");
+		scanf("%f", &salario);
+		printf("Quantos anos trabalha na empresa?");
+		scanf("%d", &servico);
+		
+		if(sexo=='f')
+		{
+			fem = fem + 1;
+		}
+		else
+		{
+			if(sexo=='m')
+			{
+				hom = hom + 1;
+				salariom = salariom + salario;
+			}
+			else
+			{
+				printf("Invalido!");
+			}
+		}
+	}
+	
+	
+	printf("A quantidade de funcionárias da empresa é: %d\n", fem);
+	printf("A media salarial dos homens é: %.2f\n", salariom / hom);
+}
