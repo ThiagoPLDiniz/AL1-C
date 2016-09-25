ee71325fe98b02815dafa3d8cd61dfcebe12c8af
Thiago Diniz <thiagopaesldiniz@gmail.com>
2016-09-25 15:30:46 -0300

    Add files via upload

---
+++ Exercicio3L3(2).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+	//resolvido
+	float final, taxa, valor, juros;
+	int meses, i, anos;
+	
+	printf("Insira o valor investido:\n");
+	scanf("%f", &valor);
+	
+	printf("Insira o valor de juros:\n");
+	scanf("%f", &taxa);
+	
+	printf("Insira o tempo de investimento:\n");
+	scanf("%d", &anos);
+	
+	meses = anos*12;
+	final = valor;
+	for(i=1; i<=meses; i++)
+	{
+		juros = (final * taxa) / 100;
+		final = final +juros;
+	}
+	
+	
+	printf("%.2f", final);
+	
+}
